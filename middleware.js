2c3c2377fc8a31ae93bb6ce09a8570329b00cafe
kanza-shahid-dev <kanzashahid18@gmail.com>
2026-01-02 16:52:35 +0500

    middleware added

---
+++ middleware.js
@@ -0,0 +1,48 @@
+import { NextResponse } from "next/server";
+import { createServerClient } from "@supabase/ssr";
+
+export async function middleware(req) {
+  const res = NextResponse.next();
+
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    {
+      cookies: {
+        get: (name) => req.cookies.get(name)?.value,
+        set: (name, value, options) => {
+          res.cookies.set({ name, value, ...options });
+          const response = NextResponse.next({
+            request: {
+              headers: req.headers,
+            },
+          });
+          response.cookies.set({ name, value, ...options });
+          return response;
+        },
+        remove: (name, options) => {
+          res.cookies.delete({ name, ...options });
+          const response = NextResponse.next({
+            request: { headers: req.headers },
+          });
+          response.cookies.delete({ name, ...options });
+          return response;
+        },
+      },
+    }
+  );
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user && req.nextUrl.pathname !== "/") {
+    return NextResponse.redirect(new URL("/photos", req.url));
+  }
+
+  return res;
+}
+
+export const config = {
+  matcher: ["/", "/photos"],
+};
